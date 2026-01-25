4f3467fe4c382ad7e40ea46e2044df371ab988f4
sub1n17 <sub1n17@naver.com>
2026-01-25 14:34:38 +0900

    add type

--- src/components/travelProduct-recent/hooks.tsx
+++ src/components/travelProduct-recent/hooks.tsx
@@ -6,9 +6,18 @@ interface IRecentMobileProps {
     isOpen?: boolean;
 }
 
+interface RecentProduct {
+    _id: string;
+    name: string;
+    image: string;
+    isSold: boolean | null;
+    price: number;
+    seller: string;
+}
+
 export default function UseTravelProductRecent({ isOpen }: IRecentMobileProps = {}) {
     // 로컬스토리지는 브라우저에서만 실행돼서 서버(Node, Next, SSR)에는 없기 때문에 state에 담기
-    const [recentProduct, setRecentProduct] = useState<any[]>([]);
+    const [recentProduct, setRecentProduct] = useState<RecentProduct[]>([]);
 
     useEffect(() => {
         const recent = JSON.parse(localStorage.getItem('recent') ?? '[]');
